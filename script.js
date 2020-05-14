37e987f7040eae8427eb59c0833549439fe65ef3
Kozino <49980451+Kozino@users.noreply.github.com>
2020-05-14 17:45:18 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,168 @@
+﻿const options = document.querySelector(".options").children;
+const answerTrackerContainer=document.querySelector(".answers-tracker");
+const questionNumberSpan=document.querySelector(".question-num-value");
+const totalQuestionSpan=document.querySelector(".total-question");
+const correctAnswerSpan=document.querySelector(".correct-answers");
+const totalQuestionSpan2=document.querySelector(".total-questions2");
+const percentage=document.querySelector(".percentage");
+const question=document.querySelector(".question");
+const op1=document.querySelector(".option1");
+const op2=document.querySelector(".option2");
+const op3=document.querySelector(".option3");
+const op4=document.querySelector(".option4");
+let questionIndex;
+let index=0;
+let myArray=[];
+let myArr=[];
+let score=0;
+
+
+//questions and options and answers
+
+const questions=[
+{
+	q:'Which of these animals can fly?',
+	options:['cow','chicken', 'turkey', 'dove'],
+	answer:3
+},
+	{
+	q:'Which country is the most populated in Africa',
+	options:['Nigeria','Egypt', 'China', 'Ghana'],
+	answer:0
+},
+{
+	q:'Which bird can talk',
+	options:['dodo','dove', 'parrot', 'chicken'],
+	answer:2
+},
+{
+	q:'Which laptop was the first in the world',
+	options:['IBM','Toshiba', 'Samsung', 'Lenovo'],
+	answer:1
+},
+{
+	q:'Which of this animal is domestic',
+	options:['Lion','Dog', 'Ostrich', 'Crocodile'],
+	answer:1
+}
+]
+
+//set questions and options and question number
+totalQuestionSpan.innerHTML=questions.length;
+function load(){
+	questionNumberSpan.innerHTML=index+1;
+	question.innerHTML=questions[questionIndex].q;
+	op1.innerHTML=questions[questionIndex].options[0];
+	op2.innerHTML=questions[questionIndex].options[1];
+	op3.innerHTML=questions[questionIndex].options[2];
+	op4.innerHTML=questions[questionIndex].options[3];
+	index++;
+}
+
+	function check(element){
+		if(element.id==questions[questionIndex].answer){
+			element.classList.add("correct");
+			updateAnswerTracker("correct");
+			score++;
+			console.log("score:"+score)
+		}
+		else{
+			element.classList.add("wrong");
+			updateAnswerTracker("wrong");
+		}
+
+		disabledOptions()
+	}
+
+	function disabledOptions(){
+		for(let i=0; i<options.length; i++){
+			options[i].classList.add("disabled");
+			if (options[i].id==questions[questionIndex].answer){
+				options[i].classList.add("correct");
+			}
+		}
+	}
+	function enableOptions(){
+		for(let i=0; i<options.length; i++){
+			options[i].classList.remove("disabled","correct","wrong");
+		
+	}
+}
+
+	function validate(){
+		if(!options[0].classList.contains("disabled")){
+			alert("Please select an answer")
+		}
+		else{
+			enableOptions();
+			randomQuestion();
+		}
+	}
+
+	function next(){
+		validate();
+	}
+	function randomQuestion(){
+		let randomNumber=Math.floor(Math.random()*questions.length);
+		let hitDuplicate=0;	
+			if(index==questions.length){
+				quizOver();
+			}
+			else{
+				if(myArray.length>0){
+					for(let i=0; i<myArray.length; i++){
+						if (myArray[i]==randomNumber) {
+							hitDuplicate=1
+							break
+						}
+					}
+					if(hitDuplicate==1){
+						randomQuestion();
+					}
+					else{
+						questionIndex=randomNumber;
+						load();
+						myArr.push(questionIndex);
+					}
+
+				}
+				if(myArray.length==0){
+					questionIndex=randomNumber;
+					load();
+					myArr.push(questionIndex);
+				}
+			console.log("myArr:"+myArr);
+			myArray.push(questionIndex);
+			
+		}
+	}
+
+	function answerTracker(){
+		for(let i=0; i<questions.length; i++){
+			const div=document.createElement("div")
+			answerTrackerContainer.appendChild(div);
+		}
+
+	}
+
+	function updateAnswerTracker(className){
+		answerTrackerContainer.children[index-1].classList.add(className);
+
+	}
+
+	function quizOver(){
+		document.querySelector(".quiz-over").classList.add("show");
+		correctAnswerSpan.innerHTML=score;
+		totalQuestionSpan2.innerHTML=questions.length;
+		percentage.innerHTML=(score/questions.length)*100 + "%";
+
+	}
+
+function tryAgain(){
+	window.location.reload();
+}
+
+window.onload=function(){
+	randomQuestion();
+	answerTracker();
+}
